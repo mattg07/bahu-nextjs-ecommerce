320baf27f674cf9feb3f03ea2b55212a2c13787b
mattg07 <matiasmgodoy00@gmail.com>
2024-10-16 13:20:40 +0200

    types fix and image

--- src/components/ProductImages.tsx
+++ src/components/ProductImages.tsx
@@ -7,8 +7,8 @@ const ProductImages = ({ items }: { items: string[] }) => {
   const [index, setIndex] = useState(0);
 
   return (
-    <div className="">
-      <div className="h-[500px] relative">
+    <div className="flex flex-col items-center justify-center">
+      <div className="h-[420px] w-[350px] relative ">
         <Image
           src={items[index]}
           alt={`Product Image ${index + 1}`}
@@ -17,10 +17,10 @@ const ProductImages = ({ items }: { items: string[] }) => {
           className="object-cover rounded-md"
         />
       </div>
-      <div className="flex justify-between gap-4 mt-8">
+      <div className="flex justify-center gap-4 mt-8 w-80">
         {items.map((item, i) => (
           <div
-            className="w-1/4 h-32 relative cursor-pointer"
+            className="w-1/4 h-28 relative cursor-pointer"
             key={i}
             onClick={() => setIndex(i)}
           >
